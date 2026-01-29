82cbeb1b7f4ed0dbe179dd83b16af788c29907e6
RAHEES55 <raheespolosys@gmail.com>
2026-01-29 11:57:14 +0530

    fix express version & scripts

--- Config/db_config.js
+++ Config/db_config.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-const connectDB= async ()=>{
-    try{
-        const uri = `${process.env.MONGO_URI}${process.env.DB_NAME}`;
-        await mongoose.connect(uri)
-        console.log("Data base Connected succesfully");
-    }catch(error){
-        console.error("Database Connection Error",error);
-        process.exit(1)
-    }
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database Connection Error:", error.message);
+  }
 };
+
 export default connectDB;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";  
+import path from "path";
 import connectDB from "./Config/db_config.js";
 import authRoutes from "./Routes/auth_routs.js";
 import corsMiddleware from "./middleware/cors_middleware.js";
-import uploadRoutes from "./Routes/upload_routes.js"
+import uploadRoutes from "./Routes/upload_routes.js";
 import deleteRoutes from "./Routes/delete_routs.js";
-import bookingRoutes from './Routes/booking_routs.js'
-import patchRouts from "./Routes/Patch_routs.js"
+import bookingRoutes from "./Routes/booking_routs.js";
+import patchRouts from "./Routes/Patch_routs.js";
+
 dotenv.config();
-connectDB();
 
 const app = express();
 
@@ -21,9 +21,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api", uploadRoutes);
 app.use("/api", deleteRoutes);
 app.use("/api", bookingRoutes);
-app.use("/api",patchRouts)
+app.use("/api", patchRouts);
+
+// test route (important)
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
 const PORT = process.env.PORT || 5000;
+
+connectDB(); // connect DB AFTER app setup
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
